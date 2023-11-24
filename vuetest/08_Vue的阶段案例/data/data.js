6744337059367b474dc56cc65daccb0ba0b62330
E1ny <109492277+E1ny@users.noreply.github.com>
2023-11-24 17:54:10 +0800

    Add files via upload

---
+++ vuetest/08_Vue的阶段案例/data/data.js
@@ -0,0 +1,23 @@
+const books = [
+    {
+        id: 1,
+        name: '《》',
+        date: '2000-01',
+        price: 55.00,
+        count: 1
+    },
+    {
+        id: 2,
+        name: '《》',
+        date: '2000-01',
+        price: 55.00,
+        count: 1
+    },
+    {
+        id: 3,
+        name: '《》',
+        date: '2000-01',
+        price: 100.00,
+        count: 1
+    }
+]
