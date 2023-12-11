15213d14075bf7156900f31fa8eae6383c56a9b8
E1ny <109492277+E1ny@users.noreply.github.com>
2023-12-11 10:58:12 +0800

    Add files via upload

---
+++ src/15_组件的v-model/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="app">
+        <!-- v-model在input中的使用 -->
+        <input v-model="message">
+        <!-- 实际上v-model做了以下两件事 -->
+        <input :value="message" @input="message = $event.target.value">
+
+        <!-- v-model在组件中的使用 -->
+        <counter v-model="appCounter"></counter>
+        <!-- 实际上v-model做了以下两件事 -->
+        <counter :modelValue="appCounter" @update:modelValue="appCounter = $event"></counter>
+
+        <!-- v-model: 可以自定义绑定名称 -->
+        <counter2 v-model:counter="appCounter" v-model:ey="appEy"></counter2>
+</div>
+</template>
+ 
+<script>
+import Counter from './Counter.vue'
+import Counter2 from './Counter2.vue'
+
+export default {
+  components: { 
+    Counter,
+    Counter2
+},
+    data() {
+        return {     
+            message: "aaa",
+            appCounter: 100,
+            appEy: "bbb"
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/15_组件的v-model/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>Counter: {{ modelValue }}</h2>
+        <button @click="changeCounter">修改Counter</button>
+   </div>
+</template>
+
+<script>
+export default {
+    // v-model中事件的固定名称
+    emits: ["@update:modelValue"],
+    props: {
+        modelValue: {
+            type: Number,
+            default: 0
+        }
+    },
+    methods: {
+        changeCounter() {
+            this.$emit("@update:modelValue", 1000)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
