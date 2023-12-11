4d379fbbcb3a51a3c529a2246d58ebc619f82f90
E1ny <109492277+E1ny@users.noreply.github.com>
2023-12-11 11:14:10 +0800

    Delete src directory

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-// import App from './App.vue'
-// import App from './11_ref获取元素组件/App.vue'
-// import App from './12_动态组件的使用/App.vue'
-import App from './13_Kepp-Alive的使用/App.vue'
-
-createApp(App).mount('#app')
