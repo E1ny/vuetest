203bc91cb06278112f977dab02bb240698dc114c
E1ny <109492277+E1ny@users.noreply.github.com>
2023-12-11 11:05:43 +0800

    Add files via upload

---
+++ About.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h2>about: {{ counter }}</h2>
+        <button @click="increment">+1</button>
+        <button @click="decrement">-1</button>
+    
+        <button @click="popularClick()">首页-流行</button>
+        <button @click="hotClick()">首页-热门</button>
+        <button @click="songClick()">首页-歌单</button>
+    </div> 
+</template>
+
+<script>
+import useCounter from '../hooks/useCounter'
+import { onMounted, ref } from 'vue'
+import useTitle  from '../hooks/useTitle'
+
+export default {
+    setup() {
+        const { counter, increment, decrement } = useCounter()
+   
+       const title = useTitle("关于")
+
+        function popularClick() {
+            title.value = "首页-流行"
+        }
+
+        function hotClick() {
+            title.value = "首页-热门"
+        }   
+
+        function songClick() {
+            title.value = "首页-歌单"
+        }
+
+        return {
+            counter,
+            increment,
+            decrement,
+            popularClick,
+            hotClick,
+            songClick
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
